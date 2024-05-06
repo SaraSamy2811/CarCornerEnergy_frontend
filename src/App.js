73cafbeea8a52059e8375a60793f15dfcbc7089f
yomnaabdo <yomna.a.daoud@gmail.com>
2024-05-06 20:07:30 +0300

    heder-user  update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/VISITOR/Pages/Home';
 import Register from './components/VISITOR/Pages/Register';
 import Login from './components/VISITOR/Pages/Login';
--- src/components/USER/HeaderForUser.js
+++ src/components/USER/HeaderForUser.js
@@ -1,145 +1,93 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from "../../assests/imges/logo.png";
-import { Dropdown, Collapse, initMDB } from "mdb-ui-kit";
-initMDB({ Dropdown, Collapse });
+// Initialization for ES Users
+import { Collapse, Ripple, initMDB } from "mdb-ui-kit";
+
+initMDB({ Collapse, Ripple });
 
 
 function HeaderForUser() {
-  return (
-    <div>
-      {/* Navbar */}
-      <nav className="navbar navbar-expand-lg navbar-light bg-body-tertiary">
-        {/* Container wrapper */}
-        <div className="container-fluid">
-          {/* Toggle button */}
-          <button
-            data-mdb-collapse-init
-            className="navbar-toggler"
-            type="button"
-            data-mdb-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <i className="fas fa-bars"></i>
-          </button>
+    return (
+        <div>
+            {/* Navbar */}
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                {/* Container wrapper */}
+                <div className="container-fluid">
+                    {/* Navbar brand */}
+                    <Link className="navbar-brand me-2" to="/user">
+                        <img
+                            src={Logo} // Corrected logo reference
+                            height="35"
+                            alt="MDB Logo"
+                            loading="lazy"
+                            style={{ marginTop: '-1px' }}
+                        />
+                    </Link>
 
-          {/* Collapsible wrapper */}
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            {/* Navbar brand */}
-            <a className="navbar-brand mt-2 mt-lg-0" href="#">
-              <img
-                src={Logo}
-                height="50"
-                alt="MDB Logo"
-                loading="lazy"
-              />
-            </a>
-          {/* Left links */}
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link to="/user/map" className="nav-link">Map</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/user/offers" className="nav-link">Offers</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/user/EnergyConsumptionPrediction" className="nav-link">Model</Link>
-            </li>
-          </ul>
-          {/* Left links */}
-          </div>
-          {/* Collapsible wrapper */}
+                    {/* Toggle button */}
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-mdb-target="#navbarButtonsExample"
+                        aria-controls="navbarButtonsExample"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <i className="fas fa-bars"></i>
+                    </button>
 
-          {/* Right elements */}
-          <div className="d-flex align-items-center">
-            {/* Icon */}
-            <a className="link-secondary me-3" href="#">
-              <i className="fas fa-shopping-cart"></i>
-            </a>
+                    {/* Collapsible wrapper */}
+                    <div className="collapse navbar-collapse" id="navbarButtonsExample">
+                        {/* Left links */}
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/user/map">Map</Link> {/* Link to Dashboard page */}
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/user/offers">Offers</Link> {/* Example of linking to another page */}
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/user/EnergyConsumptionPrediction">Model</Link> {/* Example of linking to yet another page */}
+                            </li>
+                        </ul>
 
-            {/* Notifications */}
-            <div className="dropdown">
-              <a
-                data-mdb-dropdown-init
-                className="link-secondary me-3 dropdown-toggle hidden-arrow"
-                href="#"
-                id="navbarDropdownMenuLink"
-                role="button"
-                aria-expanded="false"
-              >
-                <i className="fas fa-bell"></i>
-                <span className="badge rounded-pill badge-notification bg-danger">1</span>
-              </a>
-              <ul
-                className="dropdown-menu dropdown-menu-end"
-                aria-labelledby="navbarDropdownMenuLink"
-              >
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Some news
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another news
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </div>
-            {/* Avatar */}
-            <div className="dropdown">
-              <a
-                data-mdb-dropdown-init
-                className="dropdown-toggle d-flex align-items-center hidden-arrow"
-                href="#"
-                id="navbarDropdownMenuAvatar"
-                role="button"
-                aria-expanded="false"
-              >
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
-                  className="rounded-circle"
-                  height="25"
-                  alt="Black and White Portrait of a Man"
-                  loading="lazy"
-                />
-              </a>
-              <ul
-                className="dropdown-menu dropdown-menu-end"
-                aria-labelledby="navbarDropdownMenuAvatar"
-              >
-                <li>
-                <Link to="/user/profile" className="dropdown-item">
-                    View Profile
-                  </Link>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Settings
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Logout
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          {/* Right elements */}
+                        <div className="d-flex align-items-center">
+                            {/* <button type="button" className="btn btn-link px-3 me-2">
+                                Logout
+                            </button> */}
+                    
+
+{/* Avatar */}
+<div className="avatar me-3">
+    <Link to="/user/profile">
+        <img
+            src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (31).webp"
+            className="rounded-circle"
+            height="32"
+            alt="Avatar"
+            loading="lazy"
+        />
+    </Link>
+</div>
+
+<button type="button" className="btn btn-warning me-3">
+                                Logout
+                            </button>
+                            
+
+        
+
+                            
+                        </div>
+                    </div>
+                    {/* Collapsible wrapper */}
+                </div>
+                {/* Container wrapper */}
+            </nav>
+            {/* Navbar */}
         </div>
-        {/* Container wrapper */}
-      </nav>
-      {/* Navbar */}
-    </div>
-  );
+    );
 }
 
 export default HeaderForUser;
