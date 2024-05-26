36b7480688afbb4fc81264bfbde37172660e7f4b
Momantawy <momantawy2001@gmail.com>
2024-05-26 05:58:37 -0700

    home done

--- src/components/USER/Pages/HomeForUser.js
+++ src/components/USER/Pages/HomeForUser.js
@@ -1,28 +1,104 @@
 import React from 'react'
-//import VideoBackground from '../../videobackground/VideoBackground';
+import './HomeForUser.css'
+  // Define your questions and answers here
+  const questions = [
 
+     "Natrn valley ",
+    "Elmansora",
+    "Kafr saad",
+    "alex",
+    "6th of October City",
+    "Giza Governorate",
+    " Tanta",
+    "Sidi Gaber",
+    "Borg El Arab",
+    "Dahab",
+    "Ismailia",
+    "Sheikh Zayed City",
+    "First 6th of October",
+    " Markaz Ad Delingat",
+    "Aga",
+    "Kafr el-Sheikh",
+    "Port Fouad City",
+    "Faraskur",
+    "Metoubes",
+    "El Raml 1",
+    "Port al-Basal",
+    "El Alamein",
+    "Nuweiba",
+    "Luxor",
+    "Al Farafra",
+    "Al Faiyum Governorate Desert",
+    "Ibsheway",
+    "Belqas",
+    "El Shorouk",
+    "Al Obour",
+    "Heliopolis",
+    "Al Giza Desert",
+    "Hosh Eissa",
+    "Amreya 1"
+
+  ];
+
+  const answers = [
+    "3 charging stations",
+    "3 charging stations",
+    "3 charging stations",         
+    "2 charging stations",
+    "2 charging stations",
+    "2 charging stations",                               
+    "2 charging stations",
+    "2 charging stations",
+    "2 charging stations",
+    "2 charging stations",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+    "1 charging station",
+   
+    
+  ];
 function HomeForUser() {
   return (
-    <div >
-      
-
-
-   
-     h
-
-
-     o
-
-
-
-     m
-
-
-
-     e
-
+    <div className="home-section">
+      <h1>List of charging stations for electric vehicles in Egypt. Find the nearest EV charger for your electric car in Egypt </h1>
+      <h3>there are 48 charging stations for electric cars in Egypt :</h3>
+      <div className="home-container">
+        {/* Render each question and answer */}
+        {questions.map((question, index) => (
+          <div className="home-item" key={index}>
+            <div className="home-question">
+              {question}
+            </div>
+            <div className="home-answer">{answers[index]}</div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
+  
 }
 
 export default HomeForUser;
